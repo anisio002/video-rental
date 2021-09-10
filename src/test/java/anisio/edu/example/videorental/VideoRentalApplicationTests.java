070c0116338945d2be4709ee1d32e47a7f6f43ae
anisioEveris <anisio.junior-ext@viavarejo.com.br>
2021-09-10 18:17:16 -0300

    first commit

---
+++ src/test/java/anisio/edu/example/videorental/VideoRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package anisio.edu.example.videorental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
